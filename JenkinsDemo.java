55b866bb7686863180bf6e224107d79fa0041ce3
bhalaji123 <47515717+bhalaji123@users.noreply.github.com>
2021-03-10 10:01:08 +0530

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,5 +1,5 @@
-public class JenkinsDemo{
-public static void main(String[] args){
-System.out.println("Welcome to jenkins and github integration");
+class JenkinsDemo{
+     public static void main(String[] args){
+          System.out.println("Welcome to jenkins and github integration");
  }
 }
